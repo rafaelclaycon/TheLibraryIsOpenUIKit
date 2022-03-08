e984c3f87e633954d3b574f16589de74c236295c
Rafael Schmitt <rafael.claycon@gmail.com>
2022-03-08 19:59:46 -0300

    Initial Commit

---
+++ TheLibraryIsOpenUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheLibraryIsOpenUIKit
+//
+//  Created by Rafael Schmitt on 08/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
